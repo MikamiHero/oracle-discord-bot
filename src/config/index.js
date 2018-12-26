45d3ccf17b95805e39340de142baed03b44cbe25
Ray Li <ray@Rays-MBP.lan>
2018-12-27 07:42:03 +1100

    Moved a few config variables to be read from env var

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,17 @@
 const config = require("./config.json");
 
+// Config taken from environment variables
 config.nodeEnv = process.env.NODE_ENV;
-config.botToken = config[config.nodeEnv].botToken;
+config.botToken = process.env.DISCORD_BOT_TOKEN;
+config.twitchAppClientId = process.env.TWITCH_APP_CLIENT_ID;
+config.twitchAppClientSecret = process.env.TWITCH_APP_CLIENT_SECRET;
+config.twitConsumerKey = process.env.TWITTER_CONSUMER_KEY;
+config.twitConsumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+config.twitAccessToken = process.env.TWITTER_ACCESS_TOKEN;
+config.twitAccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+
+// Config taken from config.json file
 config.discordGeneralChannel = config[config.nodeEnv].discordGeneralChannel;
-config.twitchAppClientId = config[config.nodeEnv].twitchAppClientId;
-config.twitchAppClientSecret = config[config.nodeEnv].twitchAppClientSecret;
 config.twitchDefaultChannelName = config[config.nodeEnv].twitchDefaultChannelName;
-config.twitConsumerKey = config[config.nodeEnv].consumer_key;
-config.twitConsumerSecret = config[config.nodeEnv].consumer_secret;
-config.twitAccessToken = config[config.nodeEnv].access_token;
-config.twitAccessTokenSecret = config[config.nodeEnv].access_token_secret;
 
 module.exports = config;
