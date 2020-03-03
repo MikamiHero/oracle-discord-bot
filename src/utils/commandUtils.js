5d9b62d28ad6bba9dfd37cd0986a03c23f3dc09d
MikamiHero <mikamihero@gmail.com>
2020-03-03 18:22:10 +1100

    Speedrun PB command now working (need to convert time still)

--- src/utils/commandUtils.js
+++ src/utils/commandUtils.js
@@ -1,7 +1,7 @@
 const config = require("../config");
 const moment = require("moment");
 const Discord = require("discord.js");
-const { speedrunGetLatestPBForUser } = require("../utils/speedrunUtils");
+const { speedrunGetLatestPBForUser, speedrunGetUserID } = require("../utils/speedrunUtils");
 
 // Command to get the latest PB for someone off speedrun.com
 const cmdGetLatestPBForUser = async ({ discordClient, message, args }) => {
@@ -13,7 +13,33 @@ const cmdGetLatestPBForUser = async ({ discordClient, message, args }) => {
   const username = args[0].replace(/"/g, "");
   // Calling the speedrun utility function that will fetch the data via speedrun.com API
   try {
-    const speedrunPBForUser = await speedrunGetLatestPBForUser({ username });
+    // First get the ID of the user on speedrun.com (ephemeral ID)
+    const speedrunUserID = await speedrunGetUserID({ username });
+    // If null, it means something went wrong with fetching the user data
+    if (!speedrunUserID) {
+      return message.reply("Something went wrong with getting the user.");
+    }
+    // Now making the request for their latest PB based on user ID
+    const latestSpeedrunPBForUser = await speedrunGetLatestPBForUser({ userID: speedrunUserID });
+    // If null, it means something went wrong with fetching the user's PBs
+    if (!latestSpeedrunPBForUser) {
+      return message.reply("Something went wrong with getting the user's PBs.");
+    }
+    // Create the embed to return to the user
+    let speedrunPBEmbed = new Discord.RichEmbed();
+    speedrunPBEmbed.setColor("#775aac");
+    speedrunPBEmbed.setAuthor(`Latest PB for ${username}`);
+    speedrunPBEmbed.addField("Game", latestSpeedrunPBForUser.run.game);
+    speedrunPBEmbed.addField("Category", latestSpeedrunPBForUser.run.category, true);
+    speedrunPBEmbed.addField(
+      "Time",
+      moment.duration(latestSpeedrunPBForUser.run.times["primary_t"], "seconds").format("hh:mm:ss"),
+      true
+    );
+    speedrunPBEmbed.addField("Place", latestSpeedrunPBForUser.place, true);
+    speedrunPBEmbed.addField("Link", latestSpeedrunPBForUser.run.weblink, true);
+
+    return message.reply(speedrunPBEmbed);
   } catch (err) {
     // Log the error to the private text channel only admin can see
     console.log(err);
--- src/utils/speedrunUtils.js
+++ src/utils/speedrunUtils.js
@@ -8,9 +8,12 @@ class speedrunAPIError extends Error {
 }
 
 const speedrunAPIBaseURL = "https://www.speedrun.com/api/v1";
-const speedrunGameLookup = {
-  oot: {
+
+// Hard-coded some game lookup values because I doubt the epheremal ID of the game will change
+const speedrunGameLookup = [
+  {
     id: "j1l9qz1g",
+    name: "Zelda: Ocarina of Time",
     category: {
       "any%": "z275w5k0",
       noimww: "9d85yqdn",
@@ -20,8 +23,15 @@ const speedrunGameLookup = {
       ganonless: "9kvr802g"
     }
   },
-  sm64: { id: "o1y9wo6q" }
-};
+  { id: "o1y9wo6q", name: "Super Mario 64" },
+  {
+    id: "kdkjex1m",
+    name: "Zelda: Ocarina of Time Master Quest",
+    category: {
+      "Child Dungeons": "8249jed5"
+    }
+  }
+];
 
 // const speedrunConstructReqData = { game };
 
@@ -54,6 +64,38 @@ const speedrunGetWRForGameAndCategory = async ({ game, category }) => {
   }
 };
 
+const speedrunGetGame = async ({ game }) => {
+  try {
+    // Setting up the options for the request
+    const speedrunReqOptions = {
+      uri: `${speedrunAPIBaseURL}/games/${game}`,
+      json: true
+    };
+    // Make the request to speedrun.com API
+    const speedrunGameReq = await speedrunAPIRequest({ options: speedrunReqOptions });
+    // data->names->international is the string we want
+    return speedrunGameReq.data.names.international;
+  } catch (err) {
+    throw new speedrunAPIError(err.message);
+  }
+};
+
+const speedrunGetCategory = async ({ category }) => {
+  try {
+    // Setting up the options for the request
+    const speedrunReqOptions = {
+      uri: `${speedrunAPIBaseURL}/categories/${category}`,
+      json: true
+    };
+    // Make the request to speedrun.com API
+    const speedrunCategoryReq = await speedrunAPIRequest({ options: speedrunReqOptions });
+    // data.name is the property we want
+    return speedrunCategoryReq.data.name;
+  } catch (err) {
+    throw new speedrunAPIError(err.message);
+  }
+};
+
 const speedrunGetUserID = async ({ username }) => {
   // Setting up the options for request
   const speedrunReqOptions = {
@@ -63,18 +105,48 @@ const speedrunGetUserID = async ({ username }) => {
     },
     json: true
   };
+
   try {
     // Making the requets to retrieve user ID based on username
-    const speedrunUserID = await speedrunAPIRequest({ options: speedrunReqOptions });
-    console.log(speedrunUserID);
+    const speedrunUserIDReq = await speedrunAPIRequest({ options: speedrunReqOptions });
+    // Checking if the data property of the return object has more than one entry
+    if (speedrunUserIDReq.data.length > 1 || speedrunUserIDReq.data.length === 0) {
+      // If it has more than one entry or no entry, return null
+      return null;
+    }
+    // Return the matching user's id
+    return speedrunUserIDReq.data[0].id;
   } catch (err) {
     throw new speedrunAPIError(err.message);
   }
 };
 
-const speedrunGetLatestPBForUser = async ({ username }) => {
-  // Setting up the options for the request
-  return await speedrunGetUserID({ username });
+const speedrunGetLatestPBForUser = async ({ userID }) => {
+  try {
+    // Setting up options to make the latest PB request from speedrun.com
+    const speedrunReqOptions = {
+      uri: `${speedrunAPIBaseURL}/users/${userID}/personal-bests`,
+      json: true
+    };
+    // Make the request to speedrun.com API
+    const speedrunPBReq = await speedrunAPIRequest({ options: speedrunReqOptions });
+    // If the data property is empty, return null
+    if (!speedrunPBReq.data || speedrunPBReq.data.length === 0) {
+      return null;
+    }
+    // Plucking out the first entry in the list (the API returns entries in date order desc)
+    let latestSpeedrunPB = speedrunPBReq.data[0];
+    // Passing the game's ID to look up string name
+    const gameName = await speedrunGetGame({ game: latestSpeedrunPB.run.game });
+    // Passing the category ID to look up string name
+    const categoryName = await speedrunGetCategory({ category: latestSpeedrunPB.run.category });
+    // Using the display name for game and category
+    latestSpeedrunPB.run.game = gameName;
+    latestSpeedrunPB.run.category = categoryName;
+    return latestSpeedrunPB;
+  } catch (err) {
+    throw new speedrunAPIError(err.message);
+  }
 };
 
-module.exports = { speedrunGetWRForGameAndCategory, speedrunGetLatestPBForUser };
+module.exports = { speedrunGetWRForGameAndCategory, speedrunGetLatestPBForUser, speedrunGetUserID };
